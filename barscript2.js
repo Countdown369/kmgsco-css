dc45e567ca2ddb39fdba2cff76300f9f50ee5e24
Countdown369 <cabrown802@gmail.com>
2018-11-29 22:24:44 -0500

    updated date
    dec 13 baby

--- barscript2.js
+++ barscript2.js
@@ -20,6 +20,6 @@ function progress(timeleft, timetotal, $element) {
     }
 };
 
-progress((1544850000 - (Math.floor(Date.now() / 1000))), 3888000, jQuery('#progressBar'));
+progress((1544698800 - (Math.floor(Date.now() / 1000))), 3888000, jQuery('#progressBar'));
 
 });
